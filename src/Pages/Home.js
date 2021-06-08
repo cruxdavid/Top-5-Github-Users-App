24a924d7223b28d361248a71c7276e6fa827c2cd
David Cruz <d.crux10@gmail.com>
2021-06-07 22:14:11 -0600

    styled homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,24 +1,30 @@
+import "../Styles/Home.css";
+
 import { Link } from "react-router-dom";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <Link style={{ paddingLeft: "10px" }} to="/Person/GrahamCampbell">
-        GrahamCampbell
-      </Link>
-      <Link style={{ paddingLeft: "10px" }} to="/Person/fabpot">
-        fabpot
-      </Link>
-      <Link style={{ paddingLeft: "10px" }} to="/Person/weierophinney">
-        weierophinney
-      </Link>
-      <Link style={{ paddingLeft: "10px" }} to="/Person/rkh">
-        rkh
-      </Link>
-      <Link style={{ paddingLeft: "10px" }} to="/Person/josh">
-        josh
-      </Link>
+      <div className="container">
+        <h1 className="title">Top 5 Github Users</h1>
+        <p className="info">Tap the username to see more information</p>
+        <Link className="link" to="/Person/GrahamCampbell">
+          <button className="button">GrahamCampbell</button>
+        </Link>
+        <Link className="link" to="/Person/fabpot">
+          <button className="button">fabpot</button>
+        </Link>
+        <Link className="link" to="/Person/weierophinney">
+          <button className="button">weierophinney</button>
+        </Link>
+        <Link className="link" to="/Person/rkh">
+          <button className="button">rkh</button>
+        </Link>
+        <Link className="link" to="/Person/josh">
+          <button className="button">josh</button>
+        </Link>
+      </div>
     </div>
   );
 };
